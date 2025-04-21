5ff1967d925f4bbb72e487880dda35f35c84a2a5
Pablo Guiñazu <Pablo_Guinazu@intranet.itresources>
2025-04-21 01:16:55 -0300

    tobotstxt obey false

--- zonaprop_scraper/settings.py
+++ zonaprop_scraper/settings.py
@@ -17,7 +17,7 @@ NEWSPIDER_MODULE = "zonaprop_scraper.spiders"
 #USER_AGENT = "zonaprop_scraper (+http://www.yourdomain.com)"
 
 # Obey robots.txt rules
-ROBOTSTXT_OBEY = True
+ROBOTSTXT_OBEY = False
 
 # Configure maximum concurrent requests performed by Scrapy (default: 16)
 #CONCURRENT_REQUESTS = 32
