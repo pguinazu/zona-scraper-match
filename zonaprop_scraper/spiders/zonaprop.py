7dc791e59a543d85049c7f93bd98bff2fd409174
Pablo Guiñazu <Pablo_Guinazu@intranet.itresources>
2025-04-21 00:09:19 -0300

    add headers to avoid 403

--- zonaprop_scraper/spiders/zonaprop.py
+++ zonaprop_scraper/spiders/zonaprop.py
@@ -10,7 +10,16 @@ class ZonapropSpider(scrapy.Spider):
             url="https://www.zonaprop.com.ar/inmuebles-alquiler.html",
             callback=self.parse,
             headers={
-                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64)"
+                "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/123.0.6312.106 Safari/537.36",
+                "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/webp,*/*;q=0.8",
+                "Accept-Language": "es-ES,es;q=0.9",
+                "Accept-Encoding": "gzip, deflate, br",
+                "Connection": "keep-alive",
+                "Upgrade-Insecure-Requests": "1",
+                "Sec-Fetch-Dest": "document",
+                "Sec-Fetch-Mode": "navigate",
+                "Sec-Fetch-Site": "none",
+                "Sec-Fetch-User": "?1"
             }
         )
 
